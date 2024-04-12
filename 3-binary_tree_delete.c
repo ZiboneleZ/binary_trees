e20011ab425189a4ea052a113a2885886cfb3385
ZiboneleZ <zibonelez47@gmail.com>
2024-04-12 15:10:22 +0200

    Light->Tunnel

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,20 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * deletes an entire binary tree
- * @tree: pointer to the root node of the tree to delete
+ * binary_tree_delete - Deletes a binary tree.
+ * @tree: A pointer to the root node of the tree to delete.
  */
-
-void binary_tree_delete(binary_tree_t *tree) 
+void binary_tree_delete(binary_tree_t *tree)
 {
-		if (tree == NULL) {
-			return;
-		}
-		if (tree->left) {
-			binary_tree_delete(tree->left);
-		}
-		if (tree->right) {
-			binary_tree_delete(tree->right);
-		}
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
 		free(tree);
+	}
 }
