75a97ce8c780beefd1799331c8ecd87f844c751c
ZiboneleZ <zibonelez47@gmail.com>
2024-04-12 14:11:23 +0200

    Light->Tunnel

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,12 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * Creates a binary tree node
- * @parent: a pointer to the parent node of the node to create
- * @value: value to put in the new node
- * Return: pointer to the new node, or NULL on failure
+ * binary_tree_node - Creates a binary tree node.
+ * @parent: A pointer to the parent of the node to create.
+ * @value: The value to put in the new node.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new node.
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
